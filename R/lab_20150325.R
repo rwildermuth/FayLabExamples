43de150baf6b2bcbc600d6698cf7008285223ad4
gavinfay <gfay42@gmail.com>
2015-04-07 17:00:59 -0400

    files updated to include likelihood profile work for Apr 8 2015

--- R/lab_20150325.R
+++ R/lab_20150325.R
@@ -152,12 +152,82 @@ objfun.conc <- function(params=c(log.startbio=1,log.r=-0.2,log.k=10),
   if (flag==0) return(nll)
 }
 
+### 8 April 2015
+#We need to turn objfun.conc into objfun.lprof!
+#first write a wrapper function that allows us to swap out which parameters
+#to minimize over: optim() needs only parameters being changed to be in parameter vector
+##
+getlikeprof <- function(init.params=c(1,-0.2,10),
+                        catch=rep(0,10),obs=rep(10,10),flag=1,lprof.par=2,
+                        lprof.par.val=log(0.5))
+{
+  new.inits <- init.params[-lprof.par]
+  objfun <- optim(par=new.inits,fn=objfun.lprof,catch=catch,obs=obs,
+                  flag=0,lprof.par=lprof.par,lprof.par.val=lprof.par.vals[iprof])
+  if (flag==1) 
+  {
+    objfun <- objfun.lprof(new.inits,catch=catch,obs=obs,
+                           flag=0,lprof.par=lprof.par,
+                           lprof.par.val=lprof.par.vals[iprof])
+  }
+  return(objfun)
+}
+
+### now (univariate) likelihood profile version of the objective function
+#
+objfun.lprof <- function(params=c(-0.2,0),
+                         catch=rep(0,10),obs=rep(10,10),flag=1,lprof.par=2,
+                         lprof.par.val=log(0.5))
+{
+  #transform parameters, map parameter profiling over to correct biol param
+  if (lprof.par==1)
+  {
+    r <- exp(params[1])
+    k <- exp(params[2])
+    start.bio <- exp(lprof.par.val)*k
+  }
+  if (lprof.par==2)
+  {
+    r <- exp(lprof.par.val)
+    k <- exp(params[2])
+    start.bio <- exp(params[1])*k
+  }
+  if (lprof.par==3)
+  {
+    r <- exp(params[2])
+    k <- exp(lprof.par.val)
+    start.bio <- exp(params[1])*k
+  }
+  
+  #predict the biomass time series
+  predict.bio <- predict.bio(start.bio,catch,r,k)
+  
+  #compute the (negative log) likelihood function
+  nll <- nll.function.conc(predict.bio,obs)
+  
+  #return the value for the negative log-likelihood
+  if (flag==1)
+  {
+    results <- NULL
+    results$nll <- nll  
+    results$predict.bio <- predict.bio
+    return(results)
+  }
+  if (flag==0) return(nll)
+}
+
+
+
+
+
 
 TheData <- read.csv("Yellowtail.csv")
 head(TheData)
 init.params <- c(log.startbio=0.25,
                  log.r=log(0.5),
                  log.k=log(200))
+init.params <- c(0.25,log(0.5),log(200))
+
 #log.q=log(0.9),
 #log.sigma=-0.2)
 obs <- TheData$Survey..kg.tow.
@@ -178,11 +248,54 @@ exp(a$par)
 
 #likelihood profile - for next meeting?
 #
+# profile for r
 lprof.par <- 2
-lprof.par.vals <- log(seq(0.1,0.8,0.05))
-a <- NULL
+lprof.par.vals <- log(seq(0.4,0.6,0.01))
+r.lprof <- rep(NA,length(lprof.par.vals))
 for (iprof in 1:length(lprof.par.vals))
-  a[[iprof]] <- optim(par=init.params,fn=objfun.lprof,catch=catch,obs=obs,
-                      flag=0,lprof.par,lprof.par.vals[iprof])
-#We need to turn objfun.conc into objfun.lprof!
+  r.lprof[iprof] <- getlikeprof(init.params=a$par,catch=catch,obs=obs,
+                            flag=0,lprof.par=lprof.par,
+                            lprof.par.val=lprof.par.vals[iprof])$value
+
+par(mfrow=c(2,2),oma=c(0,0,0,0),mar=c(4,3,1,1))
+plot(r.lprof~exp(lprof.par.vals),type='l',xlab="r")
+abline(h=a$val+1.92,lty=2,col="blue")
+
+# profile for k
+lprof.par <- 3
+lprof.par.vals <- log(seq(120,240,10))
+k.lprof <- rep(NA,length(lprof.par.vals))
+for (iprof in 1:length(lprof.par.vals))
+  k.lprof[iprof] <- getlikeprof(init.params=a$par,catch=catch,obs=obs,
+                              flag=0,lprof.par=lprof.par,
+                              lprof.par.val=lprof.par.vals[iprof])$value
+bob <- a$par
+bob[2] <- 0.
+for (iprof in 1:length(lprof.par.vals))
+  k.lprof[iprof] <- getlikeprof(init.params=bob,catch=catch,obs=obs,
+                                flag=0,lprof.par=lprof.par,
+                                lprof.par.val=lprof.par.vals[iprof])$value
+
+
+plot(k.lprof~exp(lprof.par.vals),type='l',xlab="k")
+abline(h=a$val+1.92,lty=2,col="blue")
+
+# profile for start.bio
+lprof.par <- 1
+lprof.par.vals <- log(seq(0.2,5,0.2))
+b1.lprof <- rep(NA,length(lprof.par.vals))
+for (iprof in 1:length(lprof.par.vals))
+  b1.lprof[iprof] <- getlikeprof(init.params=a$par,catch=catch,obs=obs,
+                              flag=0,lprof.par=lprof.par,
+                              lprof.par.val=lprof.par.vals[iprof])$value
+
+plot(b1.lprof~exp(lprof.par.vals),type='l',xlab="start.bio / k")
+abline(h=a$val+1.92,lty=2,col="blue")
+
+
+
+
+getlikeprof(init.params=a$par,catch=catch,obs=obs,
+            flag=1,lprof.par=lprof.par,
+            lprof.par.val=lprof.par.vals[iprof])
 
